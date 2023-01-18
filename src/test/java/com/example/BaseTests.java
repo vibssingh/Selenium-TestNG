3179ee4e4e1dc59a1b622f4635281c3c4875a6dd
Vibha Singh <87809846+vibssingh@users.noreply.github.com>
2023-01-18 23:08:50 +0000

    Update BaseTests.java

--- src/test/java/com/example/BaseTests.java
+++ src/test/java/com/example/BaseTests.java
@@ -3,6 +3,7 @@ package com.example;
 import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import io.github.bonigarcia.wdm.WebDriverManager;
